8e4fa66ef9fc04c992b0487214dc91323b3a7c55
Hendrew Gustavo <hendreewgustavo@gmail.com>
2023-11-06 11:56:49 -0300

    Crud Cliente

--- AvernusGames_Front-End/src/app/app-routing.module.ts
+++ AvernusGames_Front-End/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientesComponent } from './components/clientes/clientes.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'clientes', component:ClientesComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
