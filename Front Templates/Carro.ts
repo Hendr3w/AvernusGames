328c5868eaff94a9782a810f2016ea4e8e1fe27a
Hendr3w <113458822+Hendr3w@users.noreply.github.com>
2023-11-14 16:34:25 -0300

    Templates

---
+++ Front Templates/Carro.ts
@@ -0,0 +1,4 @@
+export class Carro {
+    placa: string = '';
+    descricao: string = '';
+}
---
+++ Front Templates/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http'; 
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { ModalModule} from 'ngx-bootstrap/modal';
+
+import { CarrosService } from './carros.service';
+import { CarrosComponent } from './components/carros/carros.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarrosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    ModalModule.forRoot()
+  ],
+  providers: [HttpClientModule, CarrosService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
